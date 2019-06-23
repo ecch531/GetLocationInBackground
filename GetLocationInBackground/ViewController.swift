59b2fd360eb3c5e558a720f7805b2264b3ddc428
Summer Chen <azure531@gmail.com>
2019-06-23 23:25:59 +0800

    Initial Commit

---
+++ GetLocationInBackground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetLocationInBackground
+//
+//  Created by 陳姿君 on 2019/6/23.
+//  Copyright © 2019 summer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
